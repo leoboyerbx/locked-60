b93cb1479538ba70017927bfa2694ee7e708149e
leoboyerbx <leoboyer.bx@gmail.com>
2019-06-19 16:30:14 +0200

    prmeier déploiement

--- assets/js/app.js
+++ assets/js/app.js
@@ -274,4 +274,20 @@ $(document).ready(() => {
 
     autoPage()
 
+
+
+    // Fonction cachée code Konami
+    var k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+        n = 0;
+    $(document).keydown(function (e) {
+        if (e.keyCode === k[n++]) {
+            if (n === k.length) {
+                alert('Konami !!!'); // à remplacer par votre code
+                n = 0;
+                return false;
+            }
+        } else {
+            n = 0;
+        }
+    });
 })
