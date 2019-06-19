bf665159233b6cf6aa9ad6ef1db61d94464ac1ab
leoboyerbx <leo.boyer@outlook.com>
2019-06-19 07:36:06 +0200

    début intersectionobserver

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 $(document).ready(() => {
+    // On prépare la page
+    $('#page').addClass('loaded')
+
+
     let startPage = true // BOoléen indiquant si on est sur la page de déart (= on n'a pas scrollé)
     function scrollToPage() {
         $("#page").addClass('visible').focus()
@@ -8,12 +12,25 @@ $(document).ready(() => {
         console.log('scrolltopage')
         startPage = false
     }
+    let incited = false
+    function incite() {
+        incited = true
+        window.setTimeout(function() {
+            $('#play-button').addClass('open')
+            window.setTimeout(function () {
+                $("#play-button").removeClass('open')
+            }, 1500)
+        }, 800)
+    }
     function scrollToStart() {
         $("#page").removeClass('visible')
         $("#intro-container").removeClass('min')
         $('.st0').removeClass('hidden')
         console.log('scrolltohome')
         startPage = true
+        if (!incited) {
+            incite()
+        }
     }
 
     window.addEventListener("wheel", ev => {
@@ -30,17 +47,38 @@ $(document).ready(() => {
     })
     $('#arrow-scroll').click(scrollToPage)
 
+
+    let observer = new IntersectionObserver(function (observables) {
+        // observables est un tableau contenant des IntersectionObserverEntry
+        console.log(observables)
+        observables.forEach(observable => {
+            if (observable.intersectionRatio > 0.5) {
+                console.log("hey")
+                observable.target.classList.remove('not-visible')
+                observer.unobserve(observable.target)
+            }
+        })
+    }, {
+        root: $('#page').get(0),
+        threshold: [0.5]
+    })
+    document.querySelectorAll('.scroll-appear').forEach(el => {
+        el.classList.add('not-visible')
+        observer.observe(el)
+    })
+
+
+
     // décompte au chargement
-    function countTo(element, value, spacing = 50) {
+    function countTo(element, value, spacing = 50, offset = 1) {
         let count = 0;
         function increaseCount() {
             if (count <= value) {
                 element.innerHTML = count
-                count++
+                count+= offset
                 setTimeout(increaseCount, (spacing*count/value))
             }
         }
-        setTimeout(increaseCount, 100)
         increaseCount()
     }
 
@@ -50,12 +88,19 @@ $(document).ready(() => {
     // Bouton play
     let newStyle = document.createElement('style')
     let widthVal = $('#play-text span').width() + 20
-    newStyle.innerText = `#play-button:hover #play-text {width: ${widthVal}px}`
+    newStyle.innerText = `#play-button:hover #play-text, #play-button.open #play-text  {width: ${widthVal}px}`
     document.head.appendChild(newStyle)
 
     // Autoplay de la vidéo
+    let bgVid = document.querySelector('#bgvid')
+    // bgVid.load()
     $('#play-button').mouseenter(function() {
-        $('#bgvid').addClass('visible').get(0).play()
+        if (!incited) incited = true
+        bgVid.play().then(_ => {
+            bgVid.classList.add('visible')
+        }).catch(error => {
+            console.error(error)
+        })
     })
         .mouseleave(function() {
         $('#bgvid').removeClass('visible').get(0).pause()
